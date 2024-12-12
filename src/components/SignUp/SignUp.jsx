99900e9603f02d542753faf5f1da138270d2f07f
KI-Anik <iamkhairul101@gmail.com>
2024-12-12 18:19:25 +0600

    password 6 character check func added

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -3,6 +3,7 @@ import auth from '../../firebase.init';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
 
 const SignUp = () => {
+    const [success, setSuccess] = useState(null)
     const [errorMessage, setErrorMessage] = useState('')
 
     const hansleSignUp = e => {
@@ -13,49 +14,64 @@ const SignUp = () => {
 
         // reset error status
         setErrorMessage('')
+        setSuccess(false)
+
+        if (password.length < 6) {
+            setErrorMessage('password should contain at least 6 character')
+            return
+        }
 
         // firebase
         createUserWithEmailAndPassword(auth, email, password)
-        .then(result => console.log(result))
-        .catch(error => {
-            console.log('ERROR ', error.message)
-            setErrorMessage(error.message)
-        })
+            .then(result => {
+                console.log(result)
+                setSuccess(true)
+            })
+            .catch(error => {
+                console.log('ERROR ', error.message)
+                setErrorMessage(error.message)
+            })
     }
     return (
         <div className="hero bg-base-200 min-h-screen">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="text-center lg:text-left">
-            <h1 className="text-5xl font-bold">Sign Up now!</h1>
-            
-          </div>
-          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-            <form onSubmit={hansleSignUp} className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" name='password' placeholder="password" className="input input-bordered" required />
-                <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                </label>
-              </div>
-              <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
-              </div>
-            </form>
-          </div>
-            {
-                errorMessage && <p className='text-red-500'>{errorMessage}</p>
-            }
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Sign Up now!</h1>
+
+                </div>
+                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+                    <form onSubmit={hansleSignUp} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Sign Up</button>
+                        </div>
+                    </form>
+                </div>
+                <div className='text-2xl font-semibold'>
+
+                    {
+                        errorMessage && <p className='text-red-500'>{errorMessage}</p>
+                    }
+                    {
+                        success && <p className='text-green-500'> Successfully Signed Up</p>
+                    }
+                </div>
+            </div>
         </div>
-      </div>
     );
 };
 
